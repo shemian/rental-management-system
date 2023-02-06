365206d678dcc20e51a36e486c34200a71661d4c
shemian <shemian092@gmail.com>
2023-02-06 10:58:04 -0800

    Admin side-navigation page for the dashboard

---
+++ resources/views/layouts/inc/admin-sidenavbar.blade.php
@@ -0,0 +1,222 @@
+<nav class="sidebar">
+    <div class="sidebar-header">
+        <a href="#" class="sidebar-brand">
+            Property<span>Ms</span>
+        </a>
+        <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="sidebar-body">
+        <ul class="nav">
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+                <a href="dashboard.html" class="nav-link">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item nav-category">Components</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#tenants" role="button" aria-expanded="false" aria-controls="tenants">
+                    <i class="link-icon" data-feather="users"></i>
+                    <span class="link-title">Tenants</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="tenants">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">View Tenants</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Create Tenant</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#landlords" role="button" aria-expanded="false" aria-controls="landlords">
+                    <i class="link-icon" data-feather="user"></i>
+                    <span class="link-title">Landlords</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="landlords">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">View Landlords</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Create Landlord</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#properties" role="button" aria-expanded="false" aria-controls="properties">
+                    <i class="link-icon" data-feather="home"></i>
+                    <span class="link-title">Properties</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="properties">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">View Properties</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Create Property</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#proprtyunits" role="button" aria-expanded="false" aria-controls="proprtyunits">
+                    <i class="link-icon" data-feather="home"></i>
+                    <span class="link-title">Property Units</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="proprtyunits">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">View Property Units</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Create Property Units</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#leases" role="button" aria-expanded="false" aria-controls="leases">
+                    <i class="link-icon" data-feather="bell"></i>
+                    <span class="link-title">Leases/Tenancy</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="leases">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">View Leases</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Create Leases</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Expiring Leases</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Terminated Leases</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#inventory" role="button" aria-expanded="false" aria-controls="inventory">
+                    <i class="link-icon" data-feather="mail"></i>
+                    <span class="link-title">Inventory</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="inventory">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">View Inventory</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Create Inventory</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#billings" role="button" aria-expanded="false" aria-controls="billings">
+                    <i class="link-icon" data-feather="mail"></i>
+                    <span class="link-title">Billings</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="billings">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Meter Billings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Billings</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item nav-category">Invoices</li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#payments" role="button" aria-expanded="false" aria-controls="payments">
+                    <i class="link-icon" data-feather="anchor"></i>
+                    <span class="link-title">Payments</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="payments">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Show Payments</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Show Receipts</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#reports" role="button" aria-expanded="false" aria-controls="reports">
+                    <i class="link-icon" data-feather="inbox"></i>
+                    <span class="link-title">Reports</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="reports">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Deposits</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Monthly Collection Report</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Unpaid Invoices</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Property Balance </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Receipts Report </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="" target="_blank" class="nav-link">
+                    <i class="link-icon" data-feather="user-plus"></i>
+                    <span class="link-title">Manage Users</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="" target="_blank" class="nav-link">
+                    <i class="link-icon" data-feather="settings"></i>
+                    <span class="link-title">Application Settings</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
