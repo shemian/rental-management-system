9b434febb488de89e067edfa9a49652f4144f895
shemian <shemian092@gmail.com>
2023-02-17 10:50:07 -0800

    Tenant Profile Migration table

---
+++ database/migrations/2023_02_17_184509_create_tenant_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenant_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('id_number')->unique();
+            $table->string('image')->nullable();
+            $table->string('image_identity')->nullable();
+            $table->string('address');
+            $table->string('occupation_status');
+            $table->string('occupation_place');
+            $table->string('emergency_contact_person');
+            $table->string('emergency_contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenant_profiles');
+    }
+};
