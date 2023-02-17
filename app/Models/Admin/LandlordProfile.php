ad85f4b23f9a61d53942d4fa31a617452358c7b8
shemian <shemian092@gmail.com>
2023-02-17 11:25:55 -0800

    Landlord Profile Model

---
+++ app/Models/Admin/LandlordProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LandlordProfile extends Model
+{
+    use HasFactory;
+    protected $table='landlord_profiles';
+    protected $fillables=[
+        'name',
+        'email',
+        'phone',
+        'id_number',
+        'image_identity',
+        'address',
+        'bank_associated',
+        'bank_account',
+    ];
+}
