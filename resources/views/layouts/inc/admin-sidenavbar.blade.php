64cc07603f6ea2d6ea761dad311fa7ef4c522111
shemian <shemian092@gmail.com>
2023-03-01 14:18:34 -0800

    Added the dashboard uri to the sidenav bar

--- resources/views/layouts/inc/admin-sidenavbar.blade.php
+++ resources/views/layouts/inc/admin-sidenavbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
     <div class="sidebar-header">
-        <a href="#" class="sidebar-brand">
+        <a href="{{route('admindashboard')}}" class="sidebar-brand">
             Property<span>Ms</span>
         </a>
         <div class="sidebar-toggler not-active">
@@ -13,7 +13,7 @@
         <ul class="nav">
             <li class="nav-item nav-category">Main</li>
             <li class="nav-item">
-                <a href="dashboard.html" class="nav-link">
+                <a href="{{route('admindashboard')}}" class="nav-link">
                     <i class="link-icon" data-feather="box"></i>
                     <span class="link-title">Dashboard</span>
                 </a>
