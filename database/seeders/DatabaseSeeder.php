0ddd68a9380bd8ac274d332d209a2271a6097daf
shemian <shemian092@gmail.com>
2023-03-07 14:55:39 -0800

    Refactored the DatabaseSeeder File

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(AdminSeeder::class);
         $this->call(RoleSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
