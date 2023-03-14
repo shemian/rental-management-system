a8355a2c0eab22adf0f853baae195a9b57bd8487
shemian <shemian092@gmail.com>
2023-03-14 02:47:35 -0700

    Added datatables cdn in master file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,7 @@
     <!-- core:css -->
     <link rel="stylesheet" href="{{ asset('assets/css/core/core.css') }}">
     <!-- endinject -->
+    <link href="https://cdn.datatables.net/v/dt/dt-1.13.3/datatables.min.css" rel="stylesheet"/>
 
     <!-- Layout styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
@@ -26,9 +27,10 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-steps/1.1.0/jquery.steps.js" ></script>
+
 
     <!-- Scripts -->
+    @yield('styles')
 
 </head>
 <body>
@@ -57,7 +59,10 @@
 <!-- core:js -->
 <script src="{{asset('assets/vendors/core/core.js')}}"></script>
 <!-- endinject -->
-
+<script
+    src="https://code.jquery.com/jquery-3.6.4.min.js"
+    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+    crossorigin="anonymous"></script>
 <!-- Plugin js for this page -->
 <script src="{{asset('assets/vendors/chartjs/Chart.min.js')}}"></script>
 <script src="{{asset('assets/vendors/jquery.flot/jquery.flot.js')}}"></script>
@@ -67,6 +72,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.4/apexcharts.min.js"></script>
 <!-- End plugin js for this page -->
 <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+
 <!-- inject:js -->
 <script src="{{asset('assets/vendors/feather-icons/feather.min.js')}}"></script>
 <script src="{{asset('assets/js/template.js')}}"></script>
@@ -75,6 +81,8 @@
 <!-- Custom js for this page -->
 <script src="{{ asset('assets/js/dashboard-light.js') }}"></script>
 <script src="{{asset('assets/js/datepicker.js') }}"></script>
+
+@yield('scripts')
 
 
 </body>
