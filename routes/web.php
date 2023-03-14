7132bc0a3f98eabe0d9257abd0e2dc7c8bdbb2e2
shemian <shemian092@gmail.com>
2023-03-14 02:44:44 -0700

    Grouped landlord routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\TenantController;
+use App\Http\Controllers\Admin\LandlordProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,9 +23,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
+Route::prefix('/admin')->middleware(['auth','isAdmin'])->group(function(){
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admindashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin_dashboard');
 
     //Tenant Routes
     Route::group(['prefix' =>'tenant'], function(){
@@ -32,6 +33,14 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
         Route::get('/create', [TenantController::class, 'create'])->name('tenant_create');
         Route::post('/', [TenantController::class, 'store'])->name('tenant_store');
         Route::get('/get-tenant', [TenantController::class, 'getTenant'])->name('tenant_list');
+    });
+
+    //landlords Routes
+    Route::group(['prefix' =>'landlord'], function(){
+        Route::get('/', [LandlordProfileController::class, 'index'])->name('landlord_index');
+        Route::get('/create', [LandlordProfileController::class, 'create'])->name('landlord_create');
+        Route::post('/', [LandlordProfileController::class, 'store'])->name('landlord-store');
+        Route::get('/get_landlord', [LandlordProfileController::class, 'getLandlord'])->name('landlord_list');
     });
 
 
